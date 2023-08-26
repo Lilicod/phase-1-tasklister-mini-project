8f9fe744a2a0666ef6e86f79d6c75f2294e4bcb5
Lilia <liliabendjeddou9@gmail.com>
2023-08-27 00:02:35 +0100

    assignment done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form")
+  
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    newToDo(e.target.todoo.value)
+    form.reset()
+    })
 });
+
+function newToDo(todo){
+  let li = document.createElement("li")
+  let btn = document.createElement("button")
+  let ul = document.getElementById("tasks")
+  const prioritySelect = document.getElementById('priority');
+  const priority = prioritySelect.value;
+
+  li.textContent = `${todo} `
+  btn.textContent = "X"
+
+  li.classList.add(priority);
+
+  li.appendChild(btn)
+  ul.appendChild(li)
+
+  btn.addEventListener("click", deleteButton)
+
+}
+
+function deleteButton(e){
+  e.target.parentNode.remove()
+}
